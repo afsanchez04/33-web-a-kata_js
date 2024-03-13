3b77a6baba17f44b6ebc647feb677dc58d74a6ce
afsanchez04 <afsanchez04@gmail.com>
2024-03-12 20:23:57 -0500

    feat: captura variables

---
+++ 05-DOM/app.js
@@ -0,0 +1,32 @@
+
+
+const titulo = document.getElementById('miTitulo')
+const btn = document.getElementById('miBtn')
+
+console.log(titulo)
+console.log(btn)
+
+
+//Los elementos del HTML son interpretados como objetos
+
+//Ejemplo de objeto tradicional
+
+/* const user = {
+    nombre: 'pepe',
+    edad: 12,
+    pais: 'España'
+}
+ */
+
+//Ejemplo de elemento de HTML (Como lo interpretaría el DOM)
+
+/* const img = {
+    src: '...',
+    width: '',
+    alt: '',
+    title: '',
+    class: '',
+    id: '',
+    style: ''
+}  */
+
