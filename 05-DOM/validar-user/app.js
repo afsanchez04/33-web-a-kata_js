cc64af770777f35c0f63508c3fdbe182aba13c95
afsanchez04 <afsanchez04@gmail.com>
2024-03-14 21:10:33 -0500

    feat: ejercicio validacion finalizado

--- 05-DOM/validar-user/app.js
+++ 05-DOM/validar-user/app.js
@@ -1,2 +1,51 @@
 
-console.log(cuentas)
+
+const loginForm = document.querySelector('#loginForm')
+const inputUser = document.querySelector('#userName')
+const inputPass = document.querySelector('#userPass')
+const userContainer = document.querySelector('#userContainer')
+
+
+//hoisting
+//return
+//Forma 2 
+loginForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const userCard = (user, saldo) => {
+        return (
+        `
+        <div class="userCard">
+            <h1>Hola ${user}</h1>
+            <small>Saldo: $${saldo}</small>
+        </div>
+        `
+        )
+    }
+
+    //console.log( userCard('ramon',10) )
+
+    //Iterar el array cuentas que está en db.js
+    for(let i = 0; i < cuentas.length; i++){
+        //console.log(cuentas[i].nombre)
+        if(cuentas[i].nombre == inputUser.value.toLowerCase() && cuentas[i].pass == inputPass.value){
+            console.log('bienvenido')
+            userContainer.innerHTML = userCard(cuentas[i].nombre,cuentas[i].saldo)
+            return //El return finaliza una función
+        }
+    }
+
+    console.log('no existes')
+    
+
+} 
+)
+
+
+//Forma 1
+/* function validarUsuario (){
+    console.log('holaMundo')    
+}
+
+loginForm.addEventListener('click',validarUsuario) */
+
--- 05-DOM/validar-user/db.js
+++ 05-DOM/validar-user/db.js
@@ -1,7 +1,7 @@
 let cuentas = [
-    { nombre: 'Mali', pass: 1234, saldo: 200 },
-    { nombre: 'Gera', pass: 5678, saldo: 290 },
-    { nombre: 'Maui', pass: 9012, saldo: 67 },
-    { nombre: 'Pepe', pass: 1234, saldo: 21 },
-    { nombre: 'Carlos', pass: 5678, saldo: 670 }
+    { nombre: 'mali', pass: 1234, saldo: 200 },
+    { nombre: 'gera', pass: 5678, saldo: 290 },
+    { nombre: 'maui', pass: 9012, saldo: 67 },
+    { nombre: 'pepe', pass: 1234, saldo: 21 },
+    { nombre: 'carlos', pass: 5678, saldo: 670 }
 ]; 
