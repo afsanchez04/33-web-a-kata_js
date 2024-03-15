f0a3b305a972f470ae8219d55669da506b5bbe9f
afsanchez04 <afsanchez04@gmail.com>
2024-03-14 19:53:55 -0500

    feat: inicio proyecto validar-user

---
+++ 05-DOM/validar-user/db.js
@@ -0,0 +1,7 @@
+let cuentas = [
+    { nombre: 'Mali', pass: 1234, saldo: 200 },
+    { nombre: 'Gera', pass: 5678, saldo: 290 },
+    { nombre: 'Maui', pass: 9012, saldo: 67 },
+    { nombre: 'Pepe', pass: 1234, saldo: 21 },
+    { nombre: 'Carlos', pass: 5678, saldo: 670 }
+]; 
